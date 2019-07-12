a32adc64612dcf109bebdb52c40283b161d8d526
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-07-12 19:59:36 -0400

    add semantic library; add control over form

--- public/js/app.js
+++ public/js/app.js
@@ -1,62 +1,78 @@
-$('.email').on("change keyup paste",
-    function(){
-        if($(this).val()){
-            $('.icon-paper-plane').addClass("next");
-        } else {
-            $('.icon-paper-plane').removeClass("next");
-        }
-    }
-);
+$ ('.email').on ('change keyup paste', function () {
+  if ($ (this).val ()) {
+    $ ('.icon-paper-plane').addClass ('next');
+  } else {
+    $ ('.icon-paper-plane').removeClass ('next');
+  }
+});
 
-$('.next-button').hover(
-    function(){
-        $(this).css('cursor', 'pointer');
-    }
-);
+$ ('.next-button').hover (function () {
+  $ (this).css ('cursor', 'pointer');
+});
 
-$('.next-button.email').click(
-    function(){
-        console.log("Something");
-        $('.email-section').addClass("fold-up");
-        $('.password-section').removeClass("folded");
-    }
-);
+$ ('.next-button.email').click (function () {
+  console.log ('Something');
+  $ ('.email-section').addClass ('fold-up');
+  $ ('.password-section').removeClass ('folded');
+});
 
-$('.password').on("change keyup paste",
-    function(){
-        if($(this).val()){
-            $('.icon-lock').addClass("next");
-        } else {
-            $('.icon-lock').removeClass("next");
-        }
-    }
-);
+$ ('#firstTimeUser').click (function () {
+  Cognito.load ('forms', {id: '2'});
+  $ ('.formMenu').hide ();
+  $ ('.existingUserCodeInput').addClass ('hidden');
+  $ ('.eventCodeError').addClass ('hidden');
+});
 
-$('.next-button').hover(
-    function(){
-        $(this).css('cursor', 'pointer');
-    }
-);
+$ ('#existingUser').click (function () {
+  $ ('.existingUserCodeInput').removeClass ('hidden');
+});
 
-$('.next-button.password').click(
-    function(){
-        console.log("Something");
-        $('.password-section').addClass("fold-up");
-        $('.repeat-password-section').removeClass("folded");
-    }
-);
+$ ('#existingUserCode').on ('input', function () {
+  $ ('.eventCodeError').addClass ('hidden');
+});
 
-$('.repeat-password').on("change keyup paste",
-    function(){
-        if($(this).val()){
-            $('.icon-repeat-lock').addClass("next");
-        } else {
-            $('.icon-repeat-lock').removeClass("next");
-        }
-    }
-);
+$ ('#searchCode').click (function () {
+  $ ('#searchCode').addClass ('loading');
+  $ ('.existingUserCodeInput').addClass ('disabled');
+  const code = $ ('#existingUserCode').val ();
+  if (code === 'abcd') {
+    Cognito.load ('forms', {id: '2'});
+    $ ('.existingUserCodeInput').addClass ('hidden');
+    $ ('.formMenu').hide ();
+  } else {
+    $ ('.existingUserCodeInput').removeClass ('disabled');
+    $ ('#searchCode').removeClass ('loading');
+    $ ('.eventCodeError').removeClass ('hidden');
+  }
+});
 
-$(':input[type="submit"]').prop('disabled', true);
+$ ('.password').on ('change keyup paste', function () {
+  if ($ (this).val ()) {
+    $ ('.icon-lock').addClass ('next');
+  } else {
+    $ ('.icon-lock').removeClass ('next');
+  }
+});
+
+$ ('.next-button').hover (function () {
+  $ (this).css ('cursor', 'pointer');
+});
+
+$ ('.next-button.password').click (function () {
+  console.log ('Something');
+  $ ('.password-section').addClass ('fold-up');
+  $ ('.repeat-password-section').removeClass ('folded');
+});
+
+$ ('.repeat-password').on ('change keyup paste', function () {
+  if ($ (this).val ()) {
+    $ ('.icon-repeat-lock').addClass ('next');
+  } else {
+    $ ('.icon-repeat-lock').removeClass ('next');
+  }
+});
+
+$ (':input[type="submit"]').prop ('disabled', true);
 
 // $('.repeat-password-section').keyup(function() {
 //   console.log('here');
@@ -66,29 +82,31 @@ $(':input[type="submit"]').prop('disabled', true);
 // });
 // });
 
-$('.next-button.repeat-password').click(
-    function(){
-        console.log("Something -in - here");
-        $(':input[type="submit"]').prop('disabled', false);
-        $('.repeat-password-section').addClass("fold-up");
-        $('.success').css("marginTop", 0);
-    }
-);
+$ ('.next-button.repeat-password').click (function () {
+  console.log ('Something -in - here');
+  $ (':input[type="submit"]').prop ('disabled', false);
+  $ ('.repeat-password-section').addClass ('fold-up');
+  $ ('.success').css ('marginTop', 0);
+});
 
 var slideIndex = 1;
-showDivs(slideIndex);
+showDivs (slideIndex);
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+function plusDivs (n) {
+  showDivs ((slideIndex += n));
 }
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length} ;
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
+function showDivs (n) {
+  var i;
+  var x = document.getElementsByClassName ('mySlides');
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[slideIndex - 1].style.display = 'block';
 }
